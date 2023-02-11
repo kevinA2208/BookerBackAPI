0d0c5aeb4b81d40ccd456a1de9e15f4be3e075b7
kevina2208 <usamo489@gmail.com>
2023-02-11 12:44:04 -0500

    Cambio en settings para desplegar render final ahora si

--- Booker_Back/settings.py
+++ Booker_Back/settings.py
@@ -144,17 +144,6 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
-
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-MEDIA_URL = '/media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
 # Default primary key field type
